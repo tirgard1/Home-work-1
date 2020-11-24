ddb8170d927bfc8c6173769057f8b8a3a8021027
tirgard1 <74863658+tirgard1@users.noreply.github.com>
2020-11-24 11:10:07 +0300

    Add files via upload

---
+++ №1/№1/1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}");
+
+        }
+    }
+}
